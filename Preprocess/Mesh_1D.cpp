afaed22f6e6d0ea7415fb769bbf6ca4cf6d1d6b3
MaitreyaLimkar <maitreya.limkar021@gmail.com>
2024-10-14 22:45:22 +0200

    Second commit

--- Preprocess/Mesh_1D.cpp
+++ Preprocess/Mesh_1D.cpp
@@ -1,3 +1,20 @@
 //
 // Created by trueno on 10/13/24.
 //
+
+#include "Mesh_1D.hpp"
+#include <algorithm>
+#include <iostream>
+
+// Constructor definition
+Mesh_1D::Mesh_1D(const int PD, const double domain_size, const int partition, const int element_order)
+    : PD(PD), partition(partition), element_order(element_order), domain_size(domain_size){}
+
+void Mesh_1D::generate_mesh(int nargout) {
+    // TODO
+}
+
+void Mesh_1D::individual(double domain_size, int partition, int degree, int PD,
+    std::vector<double> &NL, std::vector<std::vector<double> > &EL) {
+    // TODO
+}
--- Preprocess/Mesh_1D.hpp
+++ Preprocess/Mesh_1D.hpp
@@ -5,4 +5,35 @@
 #ifndef MESH_1D_HPP
 #define MESH_1D_HPP
 
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+// Mesh_1D class declaration
+class Mesh_1D {
+private:
+    // Initializing the variables
+    int PD = 0, partition = 0, element_order = 0;
+    double domain_size = 0.0;
+    std::vector<double> NL, EL_1, EL_2, EL_3;
+
+public:
+    // Constructor for Mesh_1D
+    Mesh_1D(int PD, double domain_size, int partition, int element_order);
+
+    // Generation of mesh
+    static void generate_mesh(int nargout);
+
+    // Function declaration for helper function that generates node list and element list
+    static void individual (double domain_size, int partition, int degree, int PD,
+                            std::vector<double> &NL, std::vector<std::vector<double>> &EL);
+
+    // Getter functions to retrieve mesh data
+    [[nodiscard]] std::vector<double> getNodeList() const;
+    [[nodiscard]] std::vector<double> getElementList1() const;
+    [[nodiscard]] std::vector<double> getElementList2() const;
+    [[nodiscard]] std::vector<double> getElementList3() const;
+};
+
 #endif //MESH_1D_HPP
