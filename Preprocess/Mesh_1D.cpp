2e1e0a394b9c0c4aedd056381fd544a768a17b6e
MaitreyaLimkar <maitreya.limkar021@gmail.com>
2024-10-19 12:54:05 +0200

    Third commit with Eigen

--- Preprocess/Mesh_1D.cpp
+++ Preprocess/Mesh_1D.cpp
@@ -5,6 +5,7 @@
 #include "Mesh_1D.hpp"
 #include <algorithm>
 #include <iostream>
+#include <Eigen/Dense>
 
 // Constructor definition
 Mesh_1D::Mesh_1D(const int PD, const double domain_size, const int partition, const int element_order)
@@ -15,6 +16,13 @@ void Mesh_1D::generate_mesh(int nargout) {
 }
 
 void Mesh_1D::individual(double domain_size, int partition, int degree, int PD,
-    std::vector<double> &NL, std::vector<std::vector<double> > &EL) {
-    // TODO
+    std::vector<double> &NL, std::vector<std::vector<double> > &EL)
+{
+    const int p = partition;
+
+    const int number_of_nodes =(degree*p + 1); // NoN
+    const int number_of_elements = p;          // NoE
+    const int nodes_per_element = degree + 1;
+
+    
 }
